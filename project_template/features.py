8a53c5ed04d2203a705e8091c18742ad3d2f7db3
Victor <victor.dualibi@zup.com.br>
2021-12-06 18:45:07 -0300

    add: project modules

---
+++ project_template/features.py
@@ -0,0 +1,20 @@
+
+def create_squared_column():
+    """
+    Takes the inputed column and squares it.
+    Returns
+    -------
+
+    """
+    pass
+
+
+def create_count_string_variable():
+    """
+    Takes the inputed column and counts the number of
+    characters it has.
+    Returns
+    -------
+
+    """
+    pass
---
+++ project_template/modelling.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from project_template import data_manager
+
+
+def generate_features(file_path: str):
+    """
+    Creates the processed dataframe for modelling.
+    Parameters
+    ----------
+    file_path
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def train_model(dataframe: pd.DataFrame):
+    """
+    Trains the model.
+
+    Parameters
+    ----------
+    dataframe (pd.DataFrame)
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def evaluate_model(dataframe: pd.DataFrame):
+    """
+    Evaluates the model.
+
+    Parameters
+    ----------
+    dataframe
+
+    Returns
+    -------
+
+    """
+    pass
