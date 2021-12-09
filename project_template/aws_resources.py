8ec5feb74c08771c007af16864f14d11fe8f2c7d
Victor <victor.dualibi@zup.com.br>
2021-12-09 12:40:27 -0300

    add: default aws_resources for AWS connection

---
+++ project_template/aws_resources.py
@@ -0,0 +1,15 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+    region_name=os.getenv("REGION_NAME")
+)
+
+s3_resource = boto3.resource("s3")
